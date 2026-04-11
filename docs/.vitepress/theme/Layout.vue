9e5c20d6f79f88191f8ae3ec53fd3619dea2fa2d
whatevertogo <whatevertogo@users.noreply.github.com>
2026-04-11 10:31:22 +0800

    fix: 修正文章中关于 Agent Loop 的描述，优化步骤说明

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -10,5 +10,21 @@ const { frontmatter } = useData()
   <div v-if="frontmatter.layout === 'editorial'">
     <EditorialHome />
   </div>
-  <DefaultTheme.Layout v-else />
+  <DefaultTheme.Layout v-else>
+    <template #doc-before>
+      <h1 v-if="frontmatter.title && frontmatter.layout !== 'editorial'" class="blog-post-title">{{ frontmatter.title }}</h1>
+    </template>
+  </DefaultTheme.Layout>
 </template>
+
+<style scoped>
+.blog-post-title {
+  font-family: ui-serif, Georgia, Cambria, "Times New Roman", Times, serif;
+  font-size: 2.5rem;
+  font-weight: 800;
+  line-height: 1.2;
+  color: var(--vp-c-text-1);
+  letter-spacing: -0.015em;
+  margin-bottom: 2rem;
+}
+</style>
